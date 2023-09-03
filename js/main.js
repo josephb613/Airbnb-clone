92b1a1f719e2e92620c18894bd30fa93d51e52ff
Joseph basix <josephbasix@gmail.com>
2023-09-03 03:19:51 +0100

    mise a jour majeure de ifram

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const contenu = document.getElementById("contenu");
+const buttons = document.querySelector(".buttons");
+
+contenu.addEventListener("mouseenter", () => {
+//   buttons.style.display = "flex";
+});
+
+contenu.addEventListener("mouseleave", () => {
+//   buttons.style.display = "none";
+});
