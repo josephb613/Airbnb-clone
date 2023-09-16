fc931141200faab44df870496307111ce5e83e28
Joseph basix <josephbasix@gmail.com>
2023-09-16 23:38:13 +0100

    test

--- public/README.md
+++ public/README.md
@@ -1,1 +1,2 @@
 # Airbnb-clone
+# félicitation 
--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,30 @@
-const contenu = document.getElementById("contenu");
-const buttons = document.querySelector(".buttons");
+// const contenu = document.getElementById("contenu");
+// const buttons = document.querySelector(".buttons");
 
-contenu.addEventListener("mouseenter", () => {
-//   buttons.style.display = "flex";
-});
+// contenu.addEventListener("mouseenter", () => {
+// //   buttons.style.display = "flex";
+// });
 
-contenu.addEventListener("mouseleave", () => {
-//   buttons.style.display = "none";
-});
+// contenu.addEventListener("mouseleave", () => {
+// //   buttons.style.display = "none";
+// });
+
+const un = document.getElementById("un");
+
+un.addEventListener('mousedown', adfunction)
+
+function adfunction(){
+un.style.borderBottom="1px solid black"
+
+};
+
+const deux = document.getElementById("deux");
+
+deux.addEventListener('mousedown', deuxdfunction)
+
+function deuxdfunction(){
+deux.style.borderBottom="1px solid black"
+
+};
+
+
